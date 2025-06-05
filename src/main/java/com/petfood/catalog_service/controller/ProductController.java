4fecd5922783766a0bbb26e18565b7eada6b9dad
Vitu Felipe <victor_felipe28@hotmail.com>
2025-06-05 13:04:16 -0300

    iniciando serviço catalogos com spring e mongodb

---
+++ src/main/java/com/petfood/catalog_service/controller/ProductController.java
@@ -0,0 +1,35 @@
+package com.petfood.catalog_service.controller;
+
+import com.petfood.catalog_service.model.Product;
+import com.petfood.catalog_service.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable String id) {
+        Optional<Product> product = productRepository.findById(id);
+        return product.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+}
